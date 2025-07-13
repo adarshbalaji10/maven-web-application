1bf4f1dfec758af91feef579f2da3f046cc04693
adarshbalaji10 <adarsh.balaji10@gmail.com>
2025-07-13 18:52:07 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -15,19 +15,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/employee")
 public class EmployeeService {
 
-	
-	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
-	@ResponseBody
-	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
-			throws JSONException {
+    @RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+    @ResponseBody
+    String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+            throws JSONException {
 
-		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+        JSONObject js = new JSONObject();
+        js.put("Name", "Adarsh Balaji");
+        js.put("Calling Name", "Adarsh");
+        js.put("Hobbies", "Trekking, Blogging, Exploring DevOps");
+        js.put("Places he likes", "Kumara Parvatha, Coorg, Sakleshpur");
 
-		return js.toString();
-}
+        return js.toString();
+    }
 }
